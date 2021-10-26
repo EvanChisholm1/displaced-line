2b1aa52f5b68ffd2c55f95489ebbf0cf041f50d4
Evan Chisholm <elchisholm248@gmail.com>
2021-10-25 20:23:19 -0400

    used the line displacement teqnique to generate a crazy line, next need to add a curve

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 console.log("hello world");
+function setup() {
+  createCanvas(400, 400);
+  noLoop();
+}
+
+let previousPoints;
+
+function drawPoints(points) {
+  let lastPoint = { x: 0, y: height / 2 };
+  points.forEach(point => {
+    // circle(point.x, point.y, 10);
+    line(lastPoint.x, lastPoint.y, point.x, point.y);
+    lastPoint = { x: point.x, y: point.y };
+  });
+}
+
+function draw() {
+  let points = [];
+  const numberOfPoints = 10;
+  const spaceBetween = width / numberOfPoints;
+  for (let i = 0; i < 10; i++) {
+    points.push({ y: height / 2, x: i * spaceBetween + spaceBetween });
+  }
+
+  drawPoints(points);
+
+  for (let i = 0; i < 100; i++) {
+    points = points.map(point => {
+      const shouldAdd = Math.random() > 0.5;
+      const displaceMentAmount = Math.random() * 20;
+      if (shouldAdd) {
+        return { x: point.x, y: point.y + displaceMentAmount };
+      } else {
+        return { x: point.x, y: point.y - displaceMentAmount };
+      }
+    });
+
+    drawPoints(points);
+  }
+}
